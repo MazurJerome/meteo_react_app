24bd0ab892b6dab8dfe633c5c7f11f9e238ba1da
mandara42 <jeje4247@yahoo.fr>
2023-05-31 19:09:13 +0200

    fin sash

--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -1,37 +1,61 @@
 import "../style/Prediction.css";
+import sun from "../images/icone_sun.svg";
+import rain from "../images/icone_rain.svg";
+import lightning from "../images/lightning.svg";
 
 function Prediction() {
 	return (
 		<div className="Prediction">
 			<h3>Prediction</h3>
-			<div className="Prediction_dayBday">
-				<div className="Prediction_dayBday_day-1">
-					<p className="Day-1">lundi</p>
-					<p className="temp">24°</p>
+			<div className="Prediction_cards">
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={lightning} alt="" />
+						<p className="day">lundi</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
-				<div className="Prediction_dayBday_day0">
-					<p className="Day0">mardi</p>
-					<p className="temp">26°</p>
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={sun} alt="" />
+						<p className="day">mardi</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
-				<div className="Prediction_dayBday_day1">
-					<p className="Day1">mercredi</p>
-					<p className="temp">28°</p>
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={lightning} alt="" />
+						<p className="day">mercedi</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
-				<div className="Prediction_dayBday_day2">
-					<p className="Day2">jeudi</p>
-					<p className="temp">25°</p>
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={sun} alt="" />
+						<p className="day">jeudi</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
-				<div className="Prediction_dayBday_day3">
-					<p className="Day3">vendredi</p>
-					<p className="temp">22°</p>
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={rain} alt="" />
+						<p className="day">vendredi</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
-				<div className="Prediction_dayBday_day4">
-					<p className="Day4">samedi</p>
-					<p className="temp">24°</p>
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={lightning} alt="" />
+						<p className="day">samedi</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
-				<div className="Prediction_dayBday_day5">
-					<p className="Day5">dimanche</p>
-					<p className="temp">21°</p>
+				<div className="Prediction_dayBday">
+					<div className="card">
+						<img className="wheather_icon" src={lightning} alt="" />
+						<p className="day">dimanche</p>
+						<p className="temp">24°</p>
+					</div>
 				</div>
 			</div>
 		</div>
