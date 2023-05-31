3b336b49342f763e7053ad3c0fea219847327df0
mandara42 <jeje4247@yahoo.fr>
2023-05-31 18:11:23 +0200

    Fin du front accueil

--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -5,13 +5,34 @@ function Prediction() {
 		<div className="Prediction">
 			<h3>Prediction</h3>
 			<div className="Prediction_dayBday">
-				<div className="Prediction_dayBday_day-1"></div>
-				<div className="Prediction_dayBday_day0"></div>
-				<div className="Prediction_dayBday_day1"></div>
-				<div className="Prediction_dayBday_day2"></div>
-				<div className="Prediction_dayBday_day3"></div>
-				<div className="Prediction_dayBday_day4"></div>
-				<div className="Prediction_dayBday_day5"></div>
+				<div className="Prediction_dayBday_day-1">
+					<p className="Day-1">lundi</p>
+					<p className="temp">24°</p>
+				</div>
+				<div className="Prediction_dayBday_day0">
+					<p className="Day0">mardi</p>
+					<p className="temp">26°</p>
+				</div>
+				<div className="Prediction_dayBday_day1">
+					<p className="Day1">mercredi</p>
+					<p className="temp">28°</p>
+				</div>
+				<div className="Prediction_dayBday_day2">
+					<p className="Day2">jeudi</p>
+					<p className="temp">25°</p>
+				</div>
+				<div className="Prediction_dayBday_day3">
+					<p className="Day3">vendredi</p>
+					<p className="temp">22°</p>
+				</div>
+				<div className="Prediction_dayBday_day4">
+					<p className="Day4">samedi</p>
+					<p className="temp">24°</p>
+				</div>
+				<div className="Prediction_dayBday_day5">
+					<p className="Day5">dimanche</p>
+					<p className="temp">21°</p>
+				</div>
 			</div>
 		</div>
 	);
